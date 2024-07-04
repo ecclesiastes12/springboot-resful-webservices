c813786bb73b036d1f26a72a76a6a57574a3fe7a
ecclesiates12 <solomonotoo74@gmail.com>
2024-07-04 09:57:00 +0000

    general info

--- src/main/java/com/javaguides/springboot/SpringbootResfulWebservicesApplication.java
+++ src/main/java/com/javaguides/springboot/SpringbootResfulWebservicesApplication.java
@@ -5,7 +5,38 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+//@OpenAPIDefinition annotation is used to provide general information about the rest api 
+
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+			title = "Spring Boot REST API Documentation",
+			description = "Spring Boot REST API Doucmentation",
+			version = "v1.0",
+			contact = @Contact(
+					name = "Solomon",
+					email = "solomonotoo74@gmail.com",
+					url = "https://www.ecclinks.com"
+			),
+			//license information
+			license = @License(
+					name = "Apache 2.0",
+					url = "https://www.ecclinks.com/license"
+			)
+			
+	),
+	//external documentation
+	externalDocs = @ExternalDocumentation(
+			description = "Spring Boot User Management Document",
+			url = "https://www.ecclinks.com/user_management.html"
+	)
+)
 public class SpringbootResfulWebservicesApplication {
 
 	// CONFIGURE MODELMAPPER CLASS AS A SPRING BEAN FOR MAPPING JPA ENTITY OBJECT TO
